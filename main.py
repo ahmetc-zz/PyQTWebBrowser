463864cf30d17d7854778debf94a0d38ee2cb60d
Ahmet Ceylan <aceylan1919@gmail.com>
2021-12-31 11:21:21 +0300

    new versions

--- main.py
+++ main.py
@@ -1,217 +1,231 @@
-import datetime
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtPrintSupport import *
+import os
 import sys
 
-from PyQt5 import QtWebEngineWidgets
-from HistoryWindow import *
-from backList import *
 
-textFont = QFont("Century Gothic", 16)
-buttonFont = QFont("Century Gothic", 24)
-urlBarFont = QFont("Century Gothic", 16)
+# main window
+class MainWindow(QMainWindow):
+
+    # constructor
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        # creating a tab widget
+        self.tabs = QTabWidget()
+
+        # making document mode true
+        self.tabs.setDocumentMode(True)
+
+        # adding action when double clicked
+        self.tabs.tabBarDoubleClicked.connect(self.tab_open_doubleclick)
+
+        # adding action when tab is changed
+        self.tabs.currentChanged.connect(self.current_tab_changed)
+
+        # making tabs closeable
+        self.tabs.setTabsClosable(True)
+
+        # adding action when tab close is requested
+        self.tabs.tabCloseRequested.connect(self.close_current_tab)
+
+        # making tabs as central widget
+        self.setCentralWidget(self.tabs)
+
+        # creating a status bar
+        self.status = QStatusBar()
+
+        # setting status bar to the main window
+        self.setStatusBar(self.status)
+
+        # creating a tool bar for navigation
+        navtb = QToolBar("Navigation")
+
+        # adding tool bar tot he main window
+        self.addToolBar(navtb)
+
+        # creating back action
+        back_btn = QAction("Back", self)
+
+        # setting status tip
+        back_btn.setStatusTip("Back to previous page")
+
+        # adding action to back button
+        # making current tab to go back
+        back_btn.triggered.connect(lambda: self.tabs.currentWidget().back())
+
+        # adding this to the navigation tool bar
+        navtb.addAction(back_btn)
+
+        # similarly adding next button
+        next_btn = QAction("Forward", self)
+        next_btn.setStatusTip("Forward to next page")
+        next_btn.triggered.connect(lambda: self.tabs.currentWidget().forward())
+        navtb.addAction(next_btn)
+
+        # similarly adding reload button
+        reload_btn = QAction("Reload", self)
+        reload_btn.setStatusTip("Reload page")
+        reload_btn.triggered.connect(lambda: self.tabs.currentWidget().reload())
+        navtb.addAction(reload_btn)
+
+        # creating home action
+        home_btn = QAction("Home", self)
+        home_btn.setStatusTip("Go home")
+
+        # adding action to home button
+        home_btn.triggered.connect(self.navigate_home)
+        navtb.addAction(home_btn)
+
+        # adding a separator
+        navtb.addSeparator()
+
+        # creating a line edit widget for URL
+        self.urlbar = QLineEdit()
+
+        # adding action to line edit when return key is pressed
+        self.urlbar.returnPressed.connect(self.navigate_to_url)
+
+        # adding line edit to tool bar
+        navtb.addWidget(self.urlbar)
+
+        # similarly adding stop action
+        stop_btn = QAction("Stop", self)
+        stop_btn.setStatusTip("Stop loading current page")
+        stop_btn.triggered.connect(lambda: self.tabs.currentWidget().stop())
+        navtb.addAction(stop_btn)
+
+        # creating first tab
+        self.add_new_tab(QUrl('http://www.google.com'), 'Homepage')
+        self.add_new_tab(QUrl("http://www.youtube.com"))
+        # showing all the components
+        self.show()
+
+        # setting window title
+        self.setWindowTitle("Geek PyQt5")
+
+    # method for adding new tab
+    def add_new_tab(self, qurl=None, label="Blank"):
+
+        # if url is blank
+        if qurl is None:
+            # creating a google url
+            qurl = QUrl('http://www.google.com')
+
+        # creating a QWebEngineView object
+        browser = QWebEngineView()
+
+        # setting url to browser
+        browser.setUrl(qurl)
+
+        # setting tab index
+        i = self.tabs.addTab(browser, label)
+        self.tabs.setCurrentIndex(i)
+
+        # adding action to the browser when url is changed
+        # update the url
+        browser.urlChanged.connect(lambda qurl, browser=browser:
+                                   self.update_urlbar(qurl, browser))
+
+        # adding action to the browser when loading is finished
+        # set the tab title
+        browser.loadFinished.connect(lambda _, i=i, browser=browser:
+                                     self.tabs.setTabText(i, browser.page().title()))
+
+    # when double clicked is pressed on tabs
+    def tab_open_doubleclick(self, i):
+
+        # checking index i.e
+        # No tab under the click
+        if i == -1:
+            # creating a new tab
+            self.add_new_tab()
+
+    # when tab is changed
+    def current_tab_changed(self, i):
+
+        # get the curl
+        qurl = self.tabs.currentWidget().url()
+
+        # update the url
+        self.update_urlbar(qurl, self.tabs.currentWidget())
+
+        # update the title
+        self.update_title(self.tabs.currentWidget())
+
+    # when tab is closed
+    def close_current_tab(self, i):
+
+        # if there is only one tab
+        if self.tabs.count() < 2:
+            # do nothing
+            return
+
+        # else remove the tab
+        self.tabs.removeTab(i)
+
+    # method for updating the title
+    def update_title(self, browser):
+
+        # if signal is not from the current tab
+        if browser != self.tabs.currentWidget():
+            # do nothing
+            return
+
+        # get the page title
+        title = self.tabs.currentWidget().page().title()
+
+        # set the window title
+        self.setWindowTitle("% s - PyQt5" % title)
+
+    # action to go to home
+    def navigate_home(self):
+
+        # go to google
+        self.tabs.currentWidget().setUrl(QUrl("http://www.google.com"))
+        self.tabs.currentWidget().setUrl(QUrl("http://www.youtube.com"))
+
+    # method for navigate to url
+    def navigate_to_url(self):
+
+        # get the line edit text
+        # convert it to QUrl object
+        q = QUrl(self.urlbar.text())
+
+        # if scheme is blank
+        if q.scheme() == "":
+            # set scheme
+            q.setScheme("http")
+
+        # set the url
+        self.tabs.currentWidget().setUrl(q)
+        self.tabs.currentWidget().setUrl(q)
+
+    # method to update the url
+    def update_urlbar(self, q, browser=None):
+
+        # If this signal is not from the current tab, ignore
+        if browser != self.tabs.currentWidget():
+            return
+
+        # set text to the url bar
+        self.urlbar.setText(q.toString())
+
+        # set cursor position
+        self.urlbar.setCursorPosition(0)
 
 
-
-
-
-class WebBrowser(QWidget):
-    def __init__(self):
-        super().__init__()
-
-        self.connection = sqlite3.connect("webBrowserDB.db")
-        self.cursor = self.connection.cursor()
-
-        # create history table
-        cursor.execute("""CREATE TABLE IF NOT EXISTS "history" (
-                   "id"	INTEGER,
-                   "title"	TEXT,
-                   "url"	TEXT,
-                   "date"	TEXT,
-               	PRIMARY KEY("id")
-               	)""")
-
-
-        verticalLayout = QVBoxLayout()
-
-        self.visitedPageList = []
-        self.forwardPageList = []
-
-        self.setWindowTitle("Web Browser")
-
-        self.backBtn = QPushButton()
-        self.backBtn.setIcon(QIcon("./back.png"))
-        self.backBtn.setIconSize(QSize(32, 32))
-        self.backBtn.setStyleSheet("background: transparent;")
-        self.backBtn.setEnabled(0) # Back button is disabled initially
-        self.backBtn.clicked.connect(self.backBtnClicked)
-
-        self.backListBtn = QPushButton("|")
-        self.backListBtn.setFixedSize(10, 40)
-        self.backListBtn.setEnabled(0) # button is disabled initially
-        self.backListBtn.clicked.connect(self.showBackList)
-
-        self.forwardBtn = QPushButton()
-        self.forwardBtn.setIcon(QIcon("./forward.png"))
-        self.forwardBtn.setIconSize(QSize(32, 32))
-        self.forwardBtn.setStyleSheet("background: transparent;")
-        self.forwardBtn.setEnabled(0) # Forward button is disabled initially
-        self.forwardBtn.clicked.connect(self.forwardBtnClicked)
-
-        self.reloadBtn = QPushButton()
-        self.reloadBtn.setIcon(QIcon("./reload.png"))
-        self.reloadBtn.setIconSize(QSize(32, 32))
-        self.reloadBtn.setStyleSheet("background: transparent;")
-        self.reloadBtn.setEnabled(0)# reload button is disabled initially
-        self.reloadBtn.clicked.connect(self.reloadPage)
-
-        self.urlBar = QLineEdit()
-        self.urlBar.setAlignment(Qt.AlignLeft)
-        self.urlBar.setFont(urlBarFont)
-        self.urlBar.setPlaceholderText("Enter a url")
-        self.urlBar.returnPressed.connect(self.goUrlMethod)
-
-        goBtn = QPushButton()
-        goBtn.setIcon(QIcon("./go2.png"))
-        goBtn.setIconSize(QSize(32, 32))
-        goBtn.setStyleSheet("background: transparent;")
-        goBtn.clicked.connect(self.goUrlMethod)
-
-        historyBtn = QPushButton("H")
-        historyBtn.clicked.connect(self.openHistory)
-
-        urlBarLayout = QHBoxLayout()
-
-        urlBarLayout.addWidget(self.backBtn)
-        urlBarLayout.addWidget(self.backListBtn)
-        urlBarLayout.addWidget(self.forwardBtn)
-        urlBarLayout.addWidget(self.reloadBtn)
-        urlBarLayout.addWidget(self.urlBar)
-        urlBarLayout.addWidget(goBtn)
-        urlBarLayout.addWidget(historyBtn)
-
-        self.page = QtWebEngineWidgets.QWebEngineView()
-        self.page.loadFinished.connect(self.add_new_url_to_visited_page_list)
-        self.page.loadFinished.connect(self.updateUrlBarTextAndTitle)
-        self.page.loadFinished.connect(self.updateHistory)
-        self.page.loadFinished.connect(self.setReloadBtnEnabled)
-
-        verticalLayout.addLayout(urlBarLayout)
-        verticalLayout.addWidget(self.page)
-
-        self.setLayout(verticalLayout)
-        verticalLayout.setContentsMargins(0, 0, 0, 0)
-        self.showMaximized()
-
-    def goUrlMethod(self):
-        url = self.urlBar.text()
-        if "http" in url:
-            self.page.load(QUrl(url))
-        else:
-            self.page.load(QUrl("https://"+url))
-
-
-        self.forwardPageList.clear()
-        self.forwardBtn.setEnabled(0)
-
-    def backBtnClicked(self):
-        # First, adding current page's url to forward page list
-        currentPageUrl = self.visitedPageList[-1][1]
-        self.forwardPageList.append(currentPageUrl)
-
-        previousPageUrl = self.visitedPageList[-2][1]
-
-        self.visitedPageList.pop(-1)
-        self.visitedPageList.pop(-1)
-
-        self.page.load(QUrl(previousPageUrl))
-
-        if len(self.visitedPageList) < 2:# if there is only 1 url remains in the list, set back button disabled
-            self.backBtn.setEnabled(0)
-            self.backListBtn.setEnabled(0)
-
-        if len(self.forwardPageList) > 0:# if there is an url in forward page list, set forward button enabled
-            self.forwardBtn.setEnabled(1)
-
-    def showBackList(self):
-        self.backListWindow = BackList(self)
-        self.backListWindow.setWindowFlags(Qt.Window | Qt.CustomizeWindowHint |Qt.FramelessWindowHint)
-        self.backListWindow.setGeometry(65, 87, 300, 200)
-        self.backListWindow.show()
-
-    def forwardBtnClicked(self):
-        targetUrl = self.forwardPageList[-1]
-        self.page.load(QUrl(targetUrl))
-        self.forwardPageList.pop(-1)
-
-        if len(self.forwardPageList) == 0:# if there is no url in forward page list, set forward button disabled
-            self.forwardBtn.setEnabled(0)
-
-    def reloadPage(self):
-
-        self.reloadBtn.setEnabled(0)
-
-        currentPageUrl = self.visitedPageList[-1]
-        self.visitedPageList.pop(-1) # Remove last url because it is going to add the same again.
-        self.page.load(QUrl(currentPageUrl))
-        self.page.loadFinished.connect(self.setReloadBtnEnabled)
-
-    def setReloadBtnEnabled(self):
-        self.reloadBtn.setEnabled(1)
-
-
-    def add_new_url_to_visited_page_list(self):
-        url = str(self.page.url())
-        url = url[19:len(url) - 2]
-        siteName = self.page.title()
-        siteInfo = (siteName, url)
-        for i in self.visitedPageList:
-            if i == siteInfo:
-                self.visitedPageList.remove(i)
-        self.visitedPageList.append(siteInfo)
-
-        if len(self.visitedPageList) > 1: #if there is two urls in the list, set back button enabled
-            self.backBtn.setEnabled(1)
-            self.backListBtn.setEnabled(1)
-
-    def updateUrlBarTextAndTitle(self):
-        url = str(self.page.url())
-        url = url[19:len(url) - 2]
-        self.urlBar.setText(url)
-        self.setWindowTitle(self.page.title())
-
-
-    def getForwardPageList(self):
-        return self.forwardPageList
-
-    def getVisitedPageList(self):
-        return self.visitedPageList
-
-    def updateHistory(self):
-        title = self.page.title()
-        url = str(self.page.url())
-        url = url[19:len(url) - 2]
-        hour = datetime.datetime.now().strftime("%X")
-        day = datetime.datetime.now().strftime("%x")
-        date = hour + " - " + day
-        #eğer url geçmişte varsa öncekini silip yenisini en sona ekliyoruz
-        data = self.cursor.execute("SELECT * FROM history")
-        siteInfoList = data.fetchall()
-
-        for i in range(len(siteInfoList)):
-            if url == siteInfoList[i][2]: #url ler eşleşirse eski urlyi sil
-                self.cursor.execute("DELETE FROM history WHERE url = ?", [url])
-
-        self.cursor.execute("INSERT INTO history (title,url,date) VALUES (?,?,?)", (title,url,date))
-        self.connection.commit()
-
-    def openHistory(self):
-        self.historyWindow = HistoryWindow(self)
-        self.historyWindow.setWindowFlags(Qt.Window | Qt.CustomizeWindowHint | Qt.FramelessWindowHint)
-        self.historyWindow.setGeometry(self.page.frameGeometry().width()-500-10, 87, 500, 500)
-        self.historyWindow.show()
-
-
-
-
+# creating a PyQt5 application
 app = QApplication(sys.argv)
-browser = WebBrowser()
-sys.exit(app.exec_())
+
+# setting name to the application
+app.setApplicationName("PyQt5")
+
+# creating MainWindow object
+window = MainWindow()
+
+# loop
+app.exec_()
